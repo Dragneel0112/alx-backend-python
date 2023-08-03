c7ed5a583db73393fcbfeda23035e9569a44ffc9
Dragneel0112 <pillayruven@gmail.com>
2023-08-03 11:28:59 +0000

    Added files for Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+A function that converts a float to string
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    Recieves a float and returns a string
+    '''
+    return str(n)
