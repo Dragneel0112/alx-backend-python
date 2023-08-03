47cbf609c573f58ea26de52789b2b28a15a89f07
Dragneel0112 <pillayruven@gmail.com>
2023-08-03 11:21:42 +0000

    Added files ofor Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+A function that provides the floor number
+'''
+
+
+def floor(a: float) -> int:
+    '''
+    Returns the floor value of a float
+    '''
+    return int(a)
