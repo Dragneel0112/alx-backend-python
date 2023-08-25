4c0eb65ff79518db57d3de17d28e3c418b7273b5
Dragneel0112 <pillayruven@gmail.com>
2023-08-25 07:58:32 +0000

    Added file for Task 4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''
+Test module for clients
+'''
+import unittest
+from typing import Dict
+from unittest.mock import (
+    MagicMock,
+    patch,
+)
+from parameterized import parameterized
+
+from client import (
+    GithubOrgClient
+)
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    '''
+    Tests GithubOrgClient function
+    '''
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
+    ])
+    @patch(
+        "client.get_json",
+    )
+    def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
+        '''
+        Tests the output for org function
+        '''
+        mocked_fxn.return_value = MagicMock(return_value=resp)
+        gh_org_client = GithubOrgClient(org)
+        self.assertEqual(gh_org_client.org(), resp)
+        mocked_fxn.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
+        )
