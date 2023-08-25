670879f74f2238d7c9bc20ed9b1bf77b6b87830d
Dragneel0112 <pillayruven@gmail.com>
2023-08-25 08:12:55 +0000

    Updated test_client.py for Task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,7 +6,8 @@ import unittest
 from typing import Dict
 from unittest.mock import (
     MagicMock,
-    patch,
+    PropertyMock,
+    patch
 )
 from parameterized import parameterized
 
@@ -36,3 +37,19 @@ class TestGithubOrgClient(unittest.TestCase):
         mocked_fxn.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org)
         )
+
+    def test_public_repos_url(self) -> None:
+        '''
+        Tests public_repos_url output
+        '''
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable=PropertyMock,
+                ) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
